49d208a93024dad438096db9b4cc0c707a48c75f
Jacob Postill <postilljacob@gmail.com>
2023-01-30 16:59:03 -0400

    setting up html

---
+++ Assets/js/script.js
@@ -0,0 +1,89 @@
+var userFormEl = document.querySelector('#user-form');
+var languageButtonsEl = document.querySelector('#language-buttons');
+var nameInputEl = document.querySelector('#username');
+var repoContainerEl = document.querySelector('#repos-container');
+var repoSearchTerm = document.querySelector('#repo-search-term');
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+
+    repoContainerEl.textContent = '';
+    nameInputEl.value = '';
+  } else {
+    alert('Please enter a ciity');
+  }
+};
+
+var buttonClickHandler = function (event) {
+  var language = event.target.getAttribute('data-language');
+
+  if (language) {
+    getFeaturedRepos(language);
+
+    repoContainerEl.textContent = '';
+  }
+};
+
+var getlocation = function (user) {
+  var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + place + '&limit=10&appid=2ca1c9e6889192710f3b59dc0b31f2bf';
+  console.log(apiUrl)
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          displayRepos(data, user);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to city');
+    });
+};
+
+var displayRepos = function (repos, searchTerm) {
+    if (repos.length === 0) {
+      repoContainerEl.textContent = 'No city found.';
+      return;
+    }
+  
+    repoSearchTerm.textContent = searchTerm;
+  
+    for (var i = 0; i < repos.length; i++) {
+      var repoName = repos[i].owner.login + '/' + repos[i].name;
+  
+      var repoEl = document.createElement('a');
+      repoEl.classList = 'list-item flex-row justify-space-between align-center';
+      repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
+  
+      var titleEl = document.createElement('span');
+      titleEl.textContent = repoName;
+  
+      repoEl.appendChild(titleEl);
+  
+      var statusEl = document.createElement('span');
+      statusEl.classList = 'flex-row align-center';
+  
+      if (repos[i].open_issues_count > 0) {
+        statusEl.innerHTML =
+          "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+      } else {
+        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+      }
+  
+      repoEl.appendChild(statusEl);
+  
+      repoContainerEl.appendChild(repoEl);
+    }
+  };
+  
+
+  
